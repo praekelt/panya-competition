c8c5c7a72b7dd05cec1382e2badb92e51763de3a
Jonathan Bydendyk <jonathan@vc-41-29-250-90.umts.vodacom.co.za>
2010-04-28 08:07:34 +0200

    added models and admin

---
+++ competition/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+
+from competition.admin import ModelBaseAdmin
+from competition.models import Competition, CompetitionOption, CompetitionEntry
+
+
+class CompetitionOptionInline(admin.TabularInline):
+    model = CompetitionOption
+    fk_name = 'competition'
+    extra = 1
+
+class CompetitionEntryInline(admin.TabularInline):
+    model = CompetitionEntry
+    fk_name = 'competition'
+    extra = 1
+    
+class CompetitionAdmin(ModelBaseAdmin):
+    inlines = (
+        CompetitionOptionInline,
+        CompetitionEntryInline
+    )
+    
+admin.site.register(Competition, CompetitionAdmin)
---
+++ competition/models.py
@@ -0,0 +1,100 @@
+from datetime import datetime
+
+from django import models
+from django.contrib.auth.models import User
+
+from content.models import BaseModel
+
+
+class Competition(ModelBase):
+    content = RichTextField()
+    start_date = models.DateField(
+        blank=True, 
+        null=True, 
+        help_text='Date the competition starts.'
+    )
+    end_date = models.DateField(
+        blank=True, 
+        null=True, 
+        help_text='Date the competition ends.'
+    )
+    question = models.CharField(
+        blank=True, 
+        null=True, 
+        max_length=255,
+        help_text='Short competition question',
+    )
+    question_blurb = RichTextField(
+        blank=True, 
+        null=True, 
+        help_text='Descriptive text elaborating on the question.'
+    )
+    correct_answer = models.CharField(
+        max_length=255,
+        blank=True, 
+        null=True, 
+        help_text='Answer used to determine winning entries.'
+    )
+    rules = RichTextField(
+        blank=True, 
+        null=True, 
+        help_text='Rules specific to this competition.',
+    )
+    
+    def is_active(self):
+        '''
+        Assert that the listing in active according to start/end dates.
+        '''
+        now = datetime.now()
+        active = True
+        
+        if self.start_date:
+            actice = self.start_date <= now
+        if self.end_date:
+            active = self.end_date >= now
+            
+        return active
+    
+    def __unicode__(self):
+        return self.title
+    
+class CompetitionOption(models.Model):
+    competition = models.ForeignKey(
+        Competition,
+        related_name='competion_options'
+    )
+    title = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Competition Option'
+        verbose_name_plural = 'Competition Options'
+    
+    def __unicode__(self):
+        return self.title
+    
+class CompetitionEntry(models.Model):
+    competition = models.ForeignKey(
+        Competition,
+        related_name='competition_entries'
+    )
+    user = models.ForeignKey(
+        User,
+        related_name='competition_entries_users'
+    )
+    answer = models.CharField(max_length=255)
+    winner = models.BooleanField(
+        help_text='Mark this competition entry as a winning entry.'
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Competition Entry'
+        verbose_name_plural = 'Competition Entries'
+        
+    def correct_answer(self):
+        if self.answer and self.competition.correct_answer:
+            return self.answer.lower() == self.competition.correct_answer.lower()
+        return False
+
+    def __unicode__(self):
+        return "%s answered %s" % (self.user.username, self.answer)
