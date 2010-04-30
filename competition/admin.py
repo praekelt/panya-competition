04a1c89ed4f8d8c571905b7e3dbe15f81e5d71be
Jonathan Bydendyk <jonathan@qoda-macbook.(none)>
2010-04-30 13:40:27 +0200

    removed inlines and competition options admin class

--- competition/admin.py
+++ competition/admin.py
@@ -1,23 +1,8 @@
 from django.contrib import admin
 
 from content.admin import ModelBaseAdmin
-from competition.models import Competition, CompetitionOption, CompetitionEntry
+from competition.models import Competition, CompetitionEntry
 
-
-class CompetitionOptionInline(admin.TabularInline):
-    model = CompetitionOption
-    fk_name = 'competition'
-    extra = 1
-
-class CompetitionEntryInline(admin.TabularInline):
-    model = CompetitionEntry
-    fk_name = 'competition'
-    extra = 1
     
-class CompetitionAdmin(ModelBaseAdmin):
-    inlines = (
-        CompetitionOptionInline,
-        CompetitionEntryInline
-    )
-    
-admin.site.register(Competition, CompetitionAdmin)
+admin.site.register(Competition, ModelBaseAdmin)
+admin.site.register(CompetitionEntry)
