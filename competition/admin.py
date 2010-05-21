e038b7f55b744b1a76e9134ff864141520b640c7
jonathan <jonathan@qoda-server.(none)>
2010-05-21 17:19:19 +0200

    added CompetionOptions to admin

--- competition/admin.py
+++ competition/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 from content.admin import ModelBaseAdmin
-from competition.models import Competition, CompetitionEntry
+from competition.models import Competition, CompetitionEntry, CompetitionOptions
 
     
 admin.site.register(Competition, ModelBaseAdmin)
 admin.site.register(CompetitionEntry)
+admin.site.register(CompetitionOptions)
