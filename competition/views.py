6ec3d153d64bea0db443c99248b72055adad966c
jonathan <jonathan@qoda-server.(none)>
2010-05-21 16:18:07 +0200

    added list and details generic views

--- competition/views.py
+++ competition/views.py
@@ -1,11 +1,11 @@
-from content.filters import IntervalOrderFilterSet
-from content.generic.views import GenericObjectDetail, GenericObjectList 
+from content.generic.views import GenericObjectList, GenericObjectDetail
 from competition.models import Competition
+from pagemenu.pagemenus import DateFieldIntervalPageMenu
 
 class ObjectList(GenericObjectList):
     def get_extra_context(self, *args, **kwargs):
         extra_context = super(ObjectList, self).get_extra_context(*args, **kwargs)
-        added_context = {'title': 'Competitions'}
+        added_context = {'title': 'Competition'}
         if extra_context:
             extra_context.update(
                 added_context,
@@ -15,22 +15,36 @@ class ObjectList(GenericObjectList):
 
         return extra_context
     
-    def get_filterset(self, request, queryset):
-        return IntervalOrderFilterSet(request.GET, queryset=queryset)
-    
+    def get_pagemenu(self, request, queryset, *args, **kwargs):
+        return None#DateFieldIntervalPageMenu(queryset=queryset, request=request, field_name='start')
+
     def get_paginate_by(self):
-        return 12
+        return 7
     
     def get_queryset(self):
-        return Competition.permitted.all()
+        return Competition.permitted.all().order_by('start_date')
 
 object_list = ObjectList()
 
 class ObjectDetail(GenericObjectDetail):
-    def get_queryset(self):
-        return Competition.permitted.all()
-    
-    def get_extra_context(self):
-        return {'title': 'Competitions'}
+    def get_extra_context(self, slug, *args, **kwargs):
+        extra_context = super(ObjectDetail, self).get_extra_context(*args, **kwargs)
+        added_context = {
+            'title': 'Competition',
+        }
+        if extra_context:
+            extra_context.update(
+                added_context,
+            )
+        else:
+            extra_context = added_context
 
+        return extra_context
+    
+    def get_pagemenu(self, request, queryset, *args, **kwargs):
+        return None
+
+    def get_queryset(self, slug):
+        return Competition.permitted.all()
+        
 object_detail = ObjectDetail()
