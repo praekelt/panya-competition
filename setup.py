7a1d4517af30a6b3de72fd979a40718c7edf6348
shaunsephton <shaunsephton@gmail.com>
2010-06-14 13:15:43 +0200

    panya rename

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-competition',
-    version='dev',
-    description='Django competition app.',
-    author='Praekelt Consulting',
+    name='panya-competition',
+    version='0.0.1',
+    description='Panya competition app.',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt International',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-competition',
+    license='BSD',
+    url='http://github.com/praekelt/panya-competition',
     packages = find_packages(),
+    install_requires = [
+        'panya',
+    ],
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Panya",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
 
