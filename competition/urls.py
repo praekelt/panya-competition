b2080baccb5e6d39bce52a56855258e258ea9cf0
shaunsephton <shaunsephton@gmail.com>
2010-06-24 08:51:03 +0200

    info rename

--- competition/urls.py
+++ competition/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns(
     'competition.views',
     url(r'^list/$', 'object_list', name='competition_object_list'),
-    url(r'^rules/$', 'option_detail', name='competition_option_detail'),
+    url(r'^info/$', 'preferences_detail', name='competition_preferences_detail'),
     url(r'^(?P<slug>[\w-]+)/$', 'object_detail', name='competition_object_detail'),
 )
